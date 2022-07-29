b4e4cebc8cff1c0cc9467bda65bbaeaf4a5e47af
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2022-07-29 17:29:36 +0900

    add: Carmichael Numbers(UVa No.10006)

---
+++ chapter2/mathematical_problem/uva10006.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 100000
+	maxBufSize     = 10000000
+)
+
+var (
+	sc = bufio.NewScanner(os.Stdin)
+	wt = bufio.NewWriter(os.Stdout)
+)
+
+func gets() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func getInt() int {
+	i, _ := strconv.Atoi(gets())
+	return i
+}
+
+func puts(a ...interface{}) {
+	fmt.Fprintln(wt, a...)
+}
+
+func modPow(a, n, mod int) int {
+	ret := 1
+	for n > 0 {
+		if n&1 == 1 {
+			ret = (ret * a) % mod
+		}
+		a = (a * a) % mod
+		n >>= 1
+	}
+	return ret
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, initialBufSize), maxBufSize)
+	defer wt.Flush()
+
+	n := getInt()
+
+	for x := 2; x < n; x++ {
+		if modPow(x, n, n) != x {
+			puts("No")
+			return
+		}
+	}
+
+	puts("Yes")
+}
