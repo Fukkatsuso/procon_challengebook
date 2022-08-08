d2afbaff0e84cc539c481911ba506bf8a742a320
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2022-08-08 20:33:14 +0900

    add: Subsequence (POJ No.3061)

---
+++ chapter3/technique/subsequence.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 100000
+	maxBufSize     = 10000000
+	inf            = 1 << 60
+)
+
+var (
+	sc = bufio.NewScanner(os.Stdin)
+	wt = bufio.NewWriter(os.Stdout)
+)
+
+func gets() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func getInt() int {
+	i, _ := strconv.Atoi(gets())
+	return i
+}
+
+func getInts(n int) []int {
+	slice := make([]int, n)
+	for i := 0; i < n; i++ {
+		slice[i] = getInt()
+	}
+	return slice
+}
+
+func puts(a ...interface{}) {
+	fmt.Fprintln(wt, a...)
+}
+
+func min(nums ...int) int {
+	ret := nums[0]
+	for _, v := range nums {
+		if v < ret {
+			ret = v
+		}
+	}
+	return ret
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, initialBufSize), maxBufSize)
+	defer wt.Flush()
+
+	n, s := getInt(), getInt()
+	a := getInts(n)
+
+	ans := inf
+	sum := 0
+	for l, r := 0, 0; l < n; l++ {
+		for sum < s && r < n {
+			sum += a[r]
+			r++
+		}
+
+		if sum >= s {
+			ans = min(ans, r-l)
+		}
+
+		sum -= a[l]
+	}
+
+	if ans == inf {
+		ans = 0
+	}
+	puts(ans)
+}
