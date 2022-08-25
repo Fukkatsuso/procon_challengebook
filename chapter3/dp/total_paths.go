23535976263b339c7c7935eceba299c6bb91450d
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2022-08-25 17:40:29 +0900

    add: グラフの長さkのパスの総数

---
+++ chapter3/dp/total_paths.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 100000
+	maxBufSize     = 10000000
+	mod            = 10007
+)
+
+var (
+	sc = bufio.NewScanner(os.Stdin)
+	wt = bufio.NewWriter(os.Stdout)
+)
+
+func gets() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func getInt() int {
+	i, _ := strconv.Atoi(gets())
+	return i
+}
+
+func getInts(n int) []int {
+	slice := make([]int, n)
+	for i := 0; i < n; i++ {
+		slice[i] = getInt()
+	}
+	return slice
+}
+
+func puts(a ...interface{}) {
+	fmt.Fprintln(wt, a...)
+}
+
+func multiply(A, B [][]int, mod int) [][]int {
+	C := make([][]int, len(A))
+	for i := range C {
+		C[i] = make([]int, len(B[0]))
+	}
+
+	for i := 0; i < len(A); i++ {
+		for j := 0; j < len(B[0]); j++ {
+			for k := 0; k < len(B); k++ {
+				C[i][j] = (C[i][j] + A[i][k]*B[k][j]) % mod
+			}
+		}
+	}
+	return C
+}
+
+func modPow(A [][]int, n, mod int) [][]int {
+	res := make([][]int, len(A))
+	for i := range res {
+		res[i] = make([]int, len(A))
+		res[i][i] = 1
+	}
+
+	for n > 0 {
+		if n&1 == 1 {
+			res = multiply(res, A, mod)
+		}
+		A = multiply(A, A, mod)
+		n >>= 1
+	}
+	return res
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, initialBufSize), maxBufSize)
+	defer wt.Flush()
+
+	n, k := getInt(), getInt()
+	G := make([][]int, n)
+	for i := range G {
+		G[i] = getInts(n)
+	}
+
+	G = modPow(G, k, mod)
+	res := 0
+	for i := range G {
+		for j := range G[i] {
+			res += G[i][j]
+		}
+	}
+
+	puts(res)
+}
