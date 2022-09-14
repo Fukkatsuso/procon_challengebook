0280004bd8b8200e723a35be3f7abdac5402442d
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2022-09-14 20:42:45 +0900

    update: Evacuation Plan (POJ No.2175)

--- chapter3/flow/evacuation_plan.go
+++ chapter3/flow/evacuation_plan.go
@@ -57,141 +57,91 @@ func main() {
 	for i := 0; i < M; i++ {
 		P[i], Q[i], C[i] = getInt(), getInt(), getInt()
 	}
-
-	// マッチンググラフを作成
-	// 0~N-1: ビル
-	// N~N+M-1: シェルター
-	s := N + M
-	t := s + 1
-	V := t + 1
-	g := NewGraph(V)
-	cost := 0 // 避難計画のコストの総和
-	F := 0    // 人の総数
+	E := make([][]int, N)
 	for i := 0; i < N; i++ {
+		E[i] = make([]int, M)
 		for j := 0; j < M; j++ {
-			e := getInt()
-			c := abs(X[i]-P[j]) + abs(Y[i]-Q[j]) + 1
-			g.AddEdge(i, N+j, INF, c)
-			cost += c * e
+			E[i][j] = getInt()
 		}
 	}
-	for i := 0; i < N; i++ {
-		g.AddEdge(s, i, B[i], 0)
-		F += B[i]
+
+	V := N + M + 1
+	// 距離行列
+	g := make([][]int, V)
+	for i := 0; i < V; i++ {
+		g[i] = make([]int, V)
+		for j := 0; j < V; j++ {
+			g[i][j] = INF
+		}
 	}
-	for i := 0; i < M; i++ {
-		g.AddEdge(N+i, t, C[i], 0)
+	for j := 0; j < M; j++ {
+		sum := 0
+		for i := 0; i < N; i++ {
+			c := abs(X[i]-P[j]) + abs(Y[i]-Q[j]) + 1
+			g[i][N+j] = c
+			if E[i][j] > 0 {
+				g[N+j][i] = -c
+			}
+			sum += E[i][j]
+		}
+		if sum > 0 {
+			g[N+M][N+j] = 0
+		}
+		if sum < C[j] {
+			g[N+j][N+M] = 0
+		}
+	}
+
+	// ワーシャルフロイド法により負閉路検出を行う
+	prev := make([][]int, V) // 最短路の直前の頂点
+	for i := 0; i < V; i++ {
+		prev[i] = make([]int, V)
+		for j := 0; j < V; j++ {
+			prev[i][j] = i
+		}
+	}
+	for k := 0; k < V; k++ {
+		for i := 0; i < V; i++ {
+			for j := 0; j < V; j++ {
+				if shorterPath := (g[i][j] > g[i][k]+g[k][j]); !shorterPath {
+					continue
+				}
+
+				g[i][j] = g[i][k] + g[k][j]
+				prev[i][j] = prev[k][j]
+
+				if negativeCycle := (i == j && g[i][i] < 0); !negativeCycle {
+					continue
+				}
+
+				// 負閉路が存在したら，閉路に沿ってフローを流す
+				used := make([]bool, V) // ループ検出フラグ
+				for v := i; !used[v]; v = prev[i][v] {
+					used[v] = true
+					if v != N+M && prev[i][v] != N+M {
+						if v >= N {
+							E[prev[i][v]][v-N]++
+						} else {
+							E[v][prev[i][v]-N]--
+						}
+					}
+				}
+
+				puts("SUBOPTIMAL")
+				for x := 0; x < N; x++ {
+					for y := 0; y < M; y++ {
+						if y == M-1 {
+							putf("%d\n", E[x][y])
+						} else {
+							putf("%d ", E[x][y])
+						}
+					}
+				}
+				return
+			}
+		}
 	}
 
 	// 最適な場合
-	if g.MinCostFlow(s, t, F) == cost {
-		puts("OPTIMAL")
-		return
-	}
-
-	// 最適でない場合
-	puts("SUBOPTIMAL")
-	for i := 0; i < N; i++ {
-		for j := 0; j < M; j++ {
-			// ビルi->シェルターj の流量は，逆辺のcapを見ればわかる
-			if j == M-1 {
-				putf("%d\n", g[N+j][i].cap)
-			} else {
-				putf("%d ", g[N+j][i].cap)
-			}
-		}
-	}
-}
-
-type Edge struct {
-	to, cap, cost, rev int
-}
-
-type Graph [][]Edge
-
-func NewGraph(v int) Graph {
-	g := make([][]Edge, v)
-	for i := range g {
-		g[i] = make([]Edge, 0)
-	}
-	return g
-}
-
-func (g Graph) AddEdge(from, to, cap, cost int) {
-	g[from] = append(g[from], Edge{
-		to:   to,
-		cap:  cap,
-		cost: cost,
-		rev:  len(g[to]),
-	})
-	g[to] = append(g[to], Edge{
-		to:   from,
-		cap:  0,
-		cost: -cost,
-		rev:  len(g[from]) - 1,
-	})
-}
-
-// sからtへの流量fの最小費用流を求める
-// 計算量はO(F|V||E|)
-// 蟻本p.203にO(F|E|log|V|) or O(F|V|^2)の改良版アルゴリズムがある
-func (g Graph) MinCostFlow(s, t, f int) int {
-	const INF = 1 << 60
-
-	prevv, preve := make([]int, len(g)), make([]int, len(g))
-
-	res := 0
-	for f > 0 {
-		// ベルマンフォード法により，s-t間最短路を求める
-		dist := make([]int, len(g))
-		for i := range dist {
-			dist[i] = INF
-		}
-		dist[s] = 0
-		update := true
-		for update {
-			update = false
-			for v := 0; v < len(g); v++ {
-				if dist[v] == INF {
-					continue
-				}
-				for i, e := range g[v] {
-					if e.cap > 0 && dist[e.to] > dist[v]+e.cost {
-						dist[e.to] = dist[v] + e.cost
-						prevv[e.to], preve[e.to] = v, i
-						update = true
-					}
-				}
-			}
-		}
-
-		// これ以上流せない
-		if dist[t] == INF {
-			return -1
-		}
-
-		// s-t間最短路に沿って目一杯流す
-		d := f
-		for v := t; v != s; v = prevv[v] {
-			d = min(d, g[prevv[v]][preve[v]].cap)
-		}
-		f -= d
-		res += d * dist[t]
-		for v := t; v != s; v = prevv[v] {
-			e := &g[prevv[v]][preve[v]]
-			e.cap -= d
-			g[v][e.rev].cap += d
-		}
-	}
-	return res
-}
-
-func min(nums ...int) int {
-	ret := nums[0]
-	for _, v := range nums {
-		if v < ret {
-			ret = v
-		}
-	}
-	return ret
+	puts("OPTIMAL")
 }
