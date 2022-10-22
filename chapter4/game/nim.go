99e476c0f8c8f6296c89e82aa9178d3e732f2ab0
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2022-10-22 19:08:33 +0900

    add: Nim

---
+++ chapter4/game/nim.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 100000
+	maxBufSize     = 10000000
+)
+
+var (
+	sc = bufio.NewScanner(os.Stdin)
+	wt = bufio.NewWriter(os.Stdout)
+)
+
+func gets() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func getInt() int {
+	i, _ := strconv.Atoi(gets())
+	return i
+}
+
+func getInts(n int) []int {
+	slice := make([]int, n)
+	for i := 0; i < n; i++ {
+		slice[i] = getInt()
+	}
+	return slice
+}
+
+func puts(a ...interface{}) {
+	fmt.Fprintln(wt, a...)
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, initialBufSize), maxBufSize)
+	defer wt.Flush()
+
+	n := getInt()
+	a := getInts(n)
+
+	x := 0
+	for i := range a {
+		x ^= a[i]
+	}
+
+	if x != 0 {
+		puts("Alice")
+	} else {
+		puts("Bob")
+	}
+}
